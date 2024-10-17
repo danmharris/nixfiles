55924438fafe88d4665b89bcfa11b2f7836f9cdd
Dan Harris <danharris1606@gmail.com>
2024-10-17 21:17:48 +0100

    feat: install rg, fzf and font

--- home.nix
+++ home.nix
@@ -15,25 +15,10 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  home.packages = with pkgs; [
+    ripgrep
+    fzf
+    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -75,6 +60,8 @@
   programs.home-manager.enable = true;
 
   catppuccin.flavor = "macchiato";
+
+  fonts.fontconfig.enable = true;
 
   programs.alacritty = {
     enable = true;
