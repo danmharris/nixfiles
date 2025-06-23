999a8c63c9c31e2f6fbf6caa007a60860e099f02
Dan Harris <danharris1606@gmail.com>
2025-06-23 19:43:03 +0100

    fix: follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {flake-parts, ...}: let
