4a39479d72bfa9b24bbff1dd1442a938dceb56d0
Dan Harris <danharris1606@gmail.com>
2025-11-15 12:41:07 +0000

    chore: pin unstable ready for 25.11 release

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/01f116e4df6a15f4ccdffb1bcd41096869fb385c";
     catppuccin.url = "github:catppuccin/nix/release-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
