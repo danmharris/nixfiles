6ae2ea1e18382deea7a4d239bdcbaab2b7a93d05
Dan Harris <danharris1606@gmail.com>
2026-01-05 20:12:41 +0000

    feat: centralise restic repos

--- nixos/modules/restic/default.nix
+++ nixos/modules/restic/default.nix
@@ -22,23 +22,43 @@ in {
       sopsFile = ./secrets.sops.yml;
     };
 
-    services.restic.backups = lib.mapAttrs (name: backupConfig:
-      {
-        repository = "/mnt/restic/${name}";
-        initialize = true;
-        passwordFile = config.sops.secrets."services/restic/password".path;
-        timerConfig = {
-          OnCalendar = "daily";
-          Persistent = true;
-          RandomizedDelaySec = "30m";
+    services.restic.backups =
+      (lib.mapAttrs (name: backupConfig:
+        {
+          repository = "/mnt/restic/apps";
+          initialize = false;
+          createWrapper = false;
+          passwordFile = config.sops.secrets."services/restic/password".path;
+          timerConfig = {
+            OnCalendar = "daily";
+            Persistent = true;
+            RandomizedDelaySec = "30m";
+          };
+          runCheck = false;
+          extraBackupArgs = [
+            "--group-by host,tags"
+            "--tag ${name}"
+          ];
+        }
+        // backupConfig)
+      cfg.backups)
+      // {
+        apps = {
+          repository = "/mnt/restic/apps";
+          initialize = true;
+          passwordFile = config.sops.secrets."services/restic/password".path;
+          timerConfig = {
+            OnCalendar = "*-*-* 4:00:00";
+            Persistent = true;
+            RandomizedDelaySec = "5m";
+          };
+          pruneOpts = [
+            "--group-by host,tags"
+            "--keep-within-daily 7d"
+            "--keep-within-weekly 1m"
+            "--keep-within-monthly 6m"
+          ];
         };
-        pruneOpts = [
-          "--keep-daily 7"
-          "--keep-weekly 4"
-          "--keep-monthly 3"
-        ];
-      }
-      // backupConfig)
-    cfg.backups;
+      };
   };
 }
