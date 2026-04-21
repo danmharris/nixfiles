1bb962beac74f1ebcadd7b99952c43802c3b66b9
Dan Harris <me@danmharris.com>
2026-04-21 18:51:45 +0100

    feat: deploy alertmanager with pushover

--- nixos/modules/prometheus/default.nix
+++ nixos/modules/prometheus/default.nix
@@ -8,8 +8,13 @@ in {
   options.mySystem.prometheus.enable = lib.mkEnableOption "prometheus";
 
   config = lib.mkIf (cfg.enable) {
+    sops.secrets."app/alertmanager/env" = {
+      sopsFile = ./secrets.sops.yml;
+    };
+
     services.prometheus = {
       enable = true;
+      webExternalUrl = "https://prometheus.dnhrrs.xyz";
 
       exporters = {
         node = {
@@ -37,12 +42,65 @@ in {
           ];
         }
       ];
+
+      alertmanagers = [
+        {
+          static_configs = [
+            {targets = ["localhost:${toString config.services.prometheus.alertmanager.port}"];}
+          ];
+        }
+      ];
+
+      alertmanager = {
+        enable = true;
+        environmentFile = config.sops.secrets."app/alertmanager/env".path;
+        webExternalUrl = "https://alertmanager.dnhrrs.xyz";
+
+        configuration = {
+          receivers = [
+            {
+              name = "pushover";
+              pushover_configs = [
+                {
+                  user_key = "$PUSHOVER_USER_KEY";
+                  token = "$PUSHOVER_TOKEN";
+                  priority = "{{ if eq .Status \"firing\" }}1{{ else }}0{{ end }}";
+                }
+              ];
+            }
+            {
+              name = "sinkhole";
+            }
+          ];
+
+          route = {
+            group_by = ["alertname" "instance"];
+            group_wait = "1m";
+            group_interval = "5m";
+            repeat_interval = "12h";
+            receiver = "pushover";
+            routes = [
+              {
+                matchers = ["alertname = PrometheusAlertManagerE2EDeadManSwitch"];
+                receiver = "sinkhole";
+              }
+            ];
+          };
+        };
+      };
     };
 
     services.caddy.virtualHosts."prometheus.dnhrrs.xyz" = {
       useACMEHost = "dnhrrs.xyz";
       extraConfig = ''
         reverse_proxy http://127.0.0.1:${toString config.services.prometheus.port}
+      '';
+    };
+
+    services.caddy.virtualHosts."alertmanager.dnhrrs.xyz" = {
+      useACMEHost = "dnhrrs.xyz";
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString config.services.prometheus.alertmanager.port}
       '';
     };
 
